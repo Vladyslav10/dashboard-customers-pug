1d009beb3726882ea7795b2abf2a7bc2e0c410fe
Vladyslav10 <vshapovalov02@gmail.com>
2022-10-02 17:28:58 +0300

    feat(main): added js

--- src/js/components/asideMenu.js
+++ src/js/components/asideMenu.js
@@ -1,5 +1,40 @@
 import 'ScssComponents/_aside_menu.scss';
 
-const asideMenu = () => {};
+const asideMenu = () => {
+	const main = document.querySelector('main');
+	const aside = document.querySelector('aside');
+	const asideItems = document.querySelectorAll('.aside__list li');
+	const links = document.querySelectorAll('.aside__link');
+	const btnBack = document.querySelector('.main__back');
+	const w = window.innerWidth;
+	console.log(w);
+	if (w > 769) {
+		asideItems[2].classList.add('active');
+	} else {
+		asideItems.forEach((el) => {
+			el.addEventListener('click', (e) => {
+				aside.classList.add('hidden');
+				main.classList.add('show');
+			});
+		});
+		btnBack.addEventListener('click', () => {
+			asideItems.forEach((el) => el.classList.remove('active'));
+			aside.classList.remove('hidden');
+			main.classList.remove('show');
+		});
+	}
+	links.forEach((el) => {
+		el.addEventListener('click', (e) => {
+			e.preventDefault();
+		});
+	});
+
+	asideItems.forEach((el) => {
+		el.addEventListener('click', (e) => {
+			asideItems.forEach((el) => el.classList.remove('active'));
+			el.classList.add('active');
+		});
+	});
+};
 
 export default asideMenu;
--- src/js/components/mainContent.js
+++ src/js/components/mainContent.js
@@ -1,5 +1,85 @@
 import 'ScssComponents/_main_content.scss';
 
-const mainContent = () => {};
+const mainContent = () => {
+	let curPage = 1;
+	const users = 320;
+	const perPage = 8;
+	let pages = Math.ceil(users / perPage);
+	const nextbtn = document.querySelector('.main__pagination-arrow.next');
+	const prevBtn = document.querySelector('.main__pagination-arrow.prev');
+	const paginationArea = document.querySelector('.main__pagination-items');
+
+	function pagCreator(pages, curPage) {
+		let spanTag = '';
+		let active;
+		let beforePage = curPage - 1;
+		let afterPage = curPage + 1;
+
+		if (curPage > 2) {
+			spanTag += '<span class="main__pagination-item">1</span>';
+			if (curPage > 3) {
+				spanTag += '<span class="dots">...</span>';
+			}
+		}
+
+		if (curPage === pages) {
+			beforePage -= 2;
+		} else if (curPage === pages - 1) {
+			beforePage -= 1;
+		}
+
+		if (curPage === 1) {
+			afterPage += 2;
+		} else if (curPage === 2) {
+			afterPage += 1;
+		}
+
+		for (let plength = beforePage; plength <= afterPage; plength += 1) {
+			// if (plength > pages) {
+			// continue;
+			// }
+			if (plength === 0) {
+				plength += 1;
+			}
+			if (curPage === plength) {
+				active = 'active';
+			} else {
+				active = '';
+			}
+			spanTag += `<span class="main__pagination-item ${active}">${plength}</span>`;
+		}
+
+		if (curPage < pages - 1) {
+			if (curPage < pages - 2) {
+				spanTag += '<span class="dots">...</span>';
+			}
+			spanTag += `<span class="main__pagination-item">${pages}</span>`;
+		}
+
+		paginationArea.innerHTML = spanTag;
+		return spanTag;
+	}
+
+	nextbtn.addEventListener('click', () => {
+		if (curPage === pages) {
+			curPage = 1;
+			pagCreator(pages, curPage);
+		} else {
+			curPage += 1;
+			pagCreator(pages, curPage);
+		}
+	});
+
+	prevBtn.addEventListener('click', () => {
+		if (curPage === 1) {
+			curPage = pages;
+			pagCreator(pages, curPage);
+		} else {
+			curPage -= 1;
+			pagCreator(pages, curPage);
+		}
+	});
+	paginationArea.innerHTML = pagCreator(pages, curPage);
+};
 
 export default mainContent;
